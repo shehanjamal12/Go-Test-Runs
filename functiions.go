63e2efa3759ae12098264dd71d746e45d566d993
shehanjamal12 <shehan@agile.financial>
2020-08-05 11:51:48 +0530

    functions test run go

---
+++ functiions.go
@@ -0,0 +1,43 @@
+
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i string
+	i = "hey there"
+	mess(i)
+	i="yo"
+	sum(i,1,2,3,4,5,6,7,8)
+	var val1 int =1
+	var val2 int =1
+	var j int=sum2(val1,val2)
+	fmt.Println(j)
+	
+	nmae:=name{
+		name:"shehan",
+	}
+	nmae.naming()
+}
+//function making
+func mess(msg string){
+	fmt.Println(msg)
+}
+
+func sum(msg string, values ...int){
+	fmt.Println(msg,values)
+} 
+//returning
+func sum2(val1 int,val2 int) (val3 int){
+	val3 = val1+val2
+	return
+}
+
+//function with struct
+type name struct{
+	name string
+}
+func (nmae name) naming(){
+	fmt.Println("hey",nmae.name)	
+}
